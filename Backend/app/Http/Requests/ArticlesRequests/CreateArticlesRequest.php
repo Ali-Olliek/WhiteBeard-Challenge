0640163980091f1b92d5e04383226c7056ec2f9e
Ali-Olliek <Olliek95@gmail.com>
2024-06-26 14:00:38 +0300

    Added Create/Update Articles Request Validators

---
+++ Backend/app/Http/Requests/ArticlesRequests/CreateArticlesRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class CreateArticlesRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Get the error messagges if validation fails
+     *
+     * @return \Response
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'messages' => $validator->errors()->all()
+            ], 400)
+        );
+    }
+}
