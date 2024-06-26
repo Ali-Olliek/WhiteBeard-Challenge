6a68088946e30116c35aa38bf44553b052b710e4
Ali-Olliek <Olliek95@gmail.com>
2024-06-26 16:46:57 +0300

    Created Database Migrations

---
+++ Backend/database/migrations/2024_06_26_133819_add_foreign_keys_to_articles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->foreign("author_id")->references("id")->on("authors");
+            $table->foreign("category_id")->references("id")->on("articles_categories");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropForeign(['author_id']);
+            $table->dropForeign(['category_id']);
+        });
+    }
+};
