226a4976d74074e19a0385ba4e50cd5f8edd0fe9
Ali-Olliek <Olliek95@gmail.com>
2024-06-26 23:01:06 +0300

    Added Articles Filter Request

---
+++ Backend/app/Http/Requests/ArticlesRequests/FilterArticlesRequest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Requests\ArticlesRequests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+
+class FilterArticlesRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "category_id" => [
+                "sometimes",
+                "integer"
+            ],
+            "author_id" => [
+                "sometimes",
+                "integer"
+            ],
+            "from" => [
+                "sometimes",
+                "lt:to",
+                "date"
+            ],
+            "to" => [
+                "sometimes",
+                "gt:from",
+                "date"
+            ]
+        ];
+    }
+
+    /**
+     * Get the error messagges if validation fails
+     *
+     * @return \Response
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'messages' => $validator->errors()->all()
+            ], 400)
+        );
+    }
+}
